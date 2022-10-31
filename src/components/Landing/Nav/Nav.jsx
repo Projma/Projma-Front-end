cd6172062417697e93662abfb27258001fb36299
Farzan Rahmani <farzanrahmani70@gmail.com>
2022-10-31 14:23:32 +0330

    [feat] adding navbar

--- src/components/Landing/Nav/Nav.jsx
+++ src/components/Landing/Nav/Nav.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Nav.css";
+import ResponsiveAppBar from "../ResponsiveAppBar/ResponsiveAppBar";
 const Nav = () => {
-  return <nav>here is the nav</nav>;
+  // return <nav>here is the nav</nav>;
+  return <ResponsiveAppBar />;
 };
 
 export default Nav;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Header from "../components/Landing/Header/Header";
+import RecipeReviewCard from "../components/Landing/Card/ComplexInteraction";
+import "../styles/Landing.css";
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 const Landing = () => {
   return (
     <div>
+      <div>
       <Header />
+      </div>
+      <div  className="centered">
+        {/* <Grid container alignItems="center" justifyItems="center" justifyContent="center" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {Array.from(Array(6)).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <RecipeReviewCard />
+            </Grid>
+          ))}
+        </Grid> */}
+        {/* <RecipeReviewCard /> */}
+      </div>
     </div>
   );
 };
