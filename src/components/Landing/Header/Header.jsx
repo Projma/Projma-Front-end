70c498d972074fcdeab6336980e667569a30f98e
Sina Alinejad <sinaalinejad4@gmail.com>
2022-10-31 10:33:39 +0330

    [chore] folders base structure

---
+++ src/components/Landing/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <Nav />
+      <h1>Projma</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Landing/Nav/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Nav.css";
+const Nav = () => {
+  return <nav>here is the nav</nav>;
+};
+
+export default Nav;
