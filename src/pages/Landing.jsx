ad2b0c64390c62a16aa49c03c26501310abce885
Sina Alinejad <sinaalinejad4@gmail.com>
2022-10-31 18:45:36 +0330

    [fix] changing cards position

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import Header from "../components/Landing/Header/Header";
 import RecipeReviewCard from "../components/Landing/Card/ComplexInteraction";
 import "../styles/Landing.css";
-import Grid from '@mui/material/Grid'; // Grid version 1
-import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Grid from "@mui/material/Grid"; // Grid version 1
+import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
 
 const Landing = () => {
   return (
     <div>
-      <div>
       <Header />
-      </div>
-      <div  className="centered">
+      <div className="cards">
         {/* <Grid container alignItems="center" justifyItems="center" justifyContent="center" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           {Array.from(Array(6)).map((_, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
@@ -19,6 +17,10 @@ const Landing = () => {
             </Grid>
           ))}
         </Grid> */}
+        <RecipeReviewCard />
+        <RecipeReviewCard />
+        <RecipeReviewCard />
+        <RecipeReviewCard />
         <RecipeReviewCard />
       </div>
     </div>
