af827ea61cf9f4acaca23adb1a9900e346102188
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2022-11-05 02:35:12 +0330

    [fix] changing input placeholder to rtl and some responsive things to header and footer

--- src/components/Landing/Footer/Footer.jsx
+++ src/components/Landing/Footer/Footer.jsx
@@ -1,129 +1,105 @@
-import React from 'react'
+import React from "react";
 import TextField from "@mui/material/TextField";
 import { Box } from "@mui/system";
 import Button from "@mui/material/Button";
 import "./Footer.css";
-import Divider from '@mui/material/Divider';
+import Divider from "@mui/material/Divider";
 const Footer = () => {
-    return (
-        <>
-            <Box
-                sx={{
-                    backgroundColor: "#91B9E3",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginTop: "10%",
-                }}
-            >
-                <Box
-                    sx={{
-                        // marginRight: "20%",
-                        // marginTop: "5%",
-                        // marginLeft: "35%",
-                        backgroundColor: "#91B9E3",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        gap: "5%",
-                        flexBasis: "58%",
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: "grid",
-                            gridTemplateColumns: "1fr",
-                            backgroundcolor: "rgb(255, 153, 0)",
-                            // color: "rgb(255, 153, 0)",
-                        }}
-                    >
-                        <h3 className='margin--5'>
-                            <b>اجتماعی</b>
-                        </h3>
-                        <h4 className='margin--5'>
-                            تلگرام
-                        </h4>
-                        <h4 className='margin--5'>
-                            اینستاگرام
-                        </h4>
-                        <h4 className='margin--5'>
-                            توییتر
-                        </h4>
-                        <h4 className='margin--5'>
-                            لینکدین
-                        </h4>
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "grid",
-                            // justifyItems: "start",
-                            gridTemplateColumns: "1fr",
-                            // backgroundColor: "rgb(240, 64, 149)",
-                        }}
-                    >
-                        <h3 className='margin--5'>
-                            تماس با ما
-                        </h3>
-                        <h4 className='margin--5'>
-                            تلفن
-                        </h4>
-                        <h4 className='margin--5'>
-                            آدرس
-                        </h4>
-                        <h4 className='margin--5'>
-                            ایمیل
-                        </h4>
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "grid",
-                            gridTemplateColumns: "1fr",
-                            // backgroundColor: "rgb(0, 210, 210)",
-                        }}
-                    >
-                        <h3 className='margin--5'>
-                            درباره ما
-                        </h3>
-                        <h4 className='margin--5'>
-                            خدمات
-                        </h4>
-                        <h4 className='margin--5'>
-                            پشتیبانی
-                        </h4>
-                        <h4 className='margin--5'>
-                            قیمت گذاری
-                        </h4>
-                    </Box>
-                </Box>
-                <Box
-                    sx={{
-                        // width: "35%",
-                        flexBasis: "42%",
-                        flexGrow: 1,
-                        marginLeft: "5%",
-                    }}>
-                    <p>
-                        با پروجما می‌توانید در یک محیط امن و آنلاین پروژه های مختلف خود را
-                        به صورت تیمی و پویا مدیریت کنید.
-                        همکاری تیمی بیشتر و ارتباطات موثر با استفاده از پروژما
-                        امکان‌پذیر می‌شود.
-                    </p>
-                </Box>
-            </Box>
-            <Divider />
-            <Box sx={{
-                color: "#E2EDF8",
-                backgroundColor: "#173A5E",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}>
-                <p className='margin--top--bottom'>
-                    All Rights Reserved © Projma.ir
-                </p>
-            </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: "#91B9E3",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "10%",
+        }}
+      >
+        <Box
+          sx={{
+            // marginRight: "20%",
+            // marginTop: "5%",
+            // marginLeft: "35%",
+            backgroundColor: "#91B9E3",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "5%",
+            flexBasis: "58%",
+          }}
+        >
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "1fr",
+              backgroundcolor: "rgb(255, 153, 0)",
+              // color: "rgb(255, 153, 0)",
+            }}
+          >
+            <h3 className="margin--5 responsive--font--size--1">
+              <b>اجتماعی</b>
+            </h3>
+            <h4 className="margin--5 responsive--font--size--1">تلگرام</h4>
+            <h4 className="margin--5 responsive--font--size--1">اینستاگرام</h4>
+            <h4 className="margin--5 responsive--font--size--1">توییتر</h4>
+            <h4 className="margin--5 responsive--font--size--1">لینکدین</h4>
+          </Box>
+          <Box
+            sx={{
+              display: "grid",
+              // justifyItems: "start",
+              gridTemplateColumns: "1fr",
+              // backgroundColor: "rgb(240, 64, 149)",
+            }}
+          >
+            <h3 className="margin--5 responsive--font--size--1">تماس با ما</h3>
+            <h4 className="margin--5 responsive--font--size--1">تلفن</h4>
+            <h4 className="margin--5 responsive--font--size--1">آدرس</h4>
+            <h4 className="margin--5 responsive--font--size--1">ایمیل</h4>
+          </Box>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "1fr",
+              // backgroundColor: "rgb(0, 210, 210)",
+            }}
+          >
+            <h3 className="margin--5 responsive--font--size--1">درباره ما</h3>
+            <h4 className="margin--5 responsive--font--size--1">خدمات</h4>
+            <h4 className="margin--5 responsive--font--size--1">پشتیبانی</h4>
+            <h4 className="margin--5 responsive--font--size--1">قیمت گذاری</h4>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            // width: "35%",
+            flexBasis: "42%",
+            flexGrow: 1,
+            marginLeft: "5%",
+          }}
+        >
+          <p className="responsive--font--size--1">
+            با پروجما می‌توانید در یک محیط امن و آنلاین پروژه های مختلف خود را
+            به صورت تیمی و پویا مدیریت کنید. همکاری تیمی بیشتر و ارتباطات موثر
+            با استفاده از پروژما امکان‌پذیر می‌شود.
+          </p>
+        </Box>
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          color: "#E2EDF8",
+          backgroundColor: "#173A5E",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <p className="margin--top--bottom">All Rights Reserved © Projma.ir</p>
+      </Box>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Landing/Header/Header.jsx
+++ src/components/Landing/Header/Header.jsx
@@ -5,31 +5,64 @@ import TextField from "@mui/material/TextField";
 import { Box } from "@mui/system";
 import Button from "@mui/material/Button";
 import x from "../../../static/images/landing1.jpg";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import StyledTextField from "../StyledTextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  direction: "rtl", // Both here and <body dir="rtl">
+});
+// Create rtl cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 const Header = () => {
   return (
     <header>
       <Nav />
       <div className="top-section">
         <div className="top-el top-el-1 top-el-img">
-          <img src={x} />
+          <img src={x} className="responsive--height" />
         </div>
         <div className="top-el top-el-2">
           <Box
             sx={{
-              marginRight: "20%",
+              marginRight: "5%",
               marginTop: "15%",
             }}
           >
-            <h1>با پروجما کیفیت کار تیمی خود را ارتقا دهید</h1>
-            <TextField
-              id="outlined-basic"
-              label="Email"
-              variant="outlined"
-              sx={{ marginX: 1 }}
-            />
+            <h1 className="responsive--font--size--2">
+              با پروجما کیفیت کار تیمی خود را ارتقا دهید
+            </h1>
+            <CacheProvider value={cacheRtl}>
+              <ThemeProvider theme={theme}>
+                <StyledTextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="ایمیل"
+                  placeholder="آدرس ایمیل خود را وارد کنید"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  sx={{ width: "60%", display: "block" }}
+                />
+              </ThemeProvider>
+            </CacheProvider>
             <Button
               variant="contained"
-              sx={{ height: 54, width: 150, fontSize: 17 }}
+              sx={{
+                height: 54,
+                width: 150,
+                fontSize: "90%",
+                width: "30%",
+                height: "100%",
+              }}
             >
               رایگان ثبت نام کنید
             </Button>
@@ -41,24 +74,3 @@ const Header = () => {
 };
 
 export default Header;
-
-{
-  /* <form>
-            <div>
-              <label for="exampleInputEmail1">ایمیل</label>
-              <input
-                type="email"
-                className="email-inp"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="ایمیل خود را وارد کنید"
-              />
-              <small id="emailHelp" className="form-text text-muted">
-                ما ایمیل شما را با هیچکس به اشتراک نخواهیم گذاشت
-              </small>
-              <button type="button" className="sign-up-btn">
-                رایگان عضو شوید
-              </button>
-            </div>
-          </form> */
-}
