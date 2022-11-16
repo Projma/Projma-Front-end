25bf773b4ab8fb03826f6b89566772828efd1426
VahidM313 <vahidood@gmail.com>
2022-11-16 23:09:31 +0330

    [fix] : board view in mobile 2

--- src/components/Board/UI/Board.js
+++ src/components/Board/UI/Board.js
@@ -5,10 +5,10 @@ import "../Styles/Board.css";
 import "../Styles/AddList.css";
 import { ratingClasses } from "@mui/material";
 
-const list_info = [];
+const listInfo = [];
 
 const Board = () => {
-  const [lists, setLists] = useState(list_info);
+  const [lists, setLists] = useState(listInfo);
 
   const clickHandler = (lists) => {
     setLists((prevLists) => {
@@ -17,11 +17,13 @@ const Board = () => {
   };
 
   return (
-      <div className="list-container">
+    <div className="list-container">
       <div className="list-container-minor">
         {lists.map(() => (
           <List />
         ))}
+      </div>
+      <div className="button-container">
         <button className="add-list-button" onClick={clickHandler}>
           <p className="add-list-button-title">+ ایجاد لیست</p>
         </button>
--- src/components/Board/UI/List.js
+++ src/components/Board/UI/List.js
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import "../Styles/List.css"
+import React, { useState, useRef } from "react";
+import "../Styles/List.css";
 import Card from "./Card";
 
-const card_info = [];
+const cardInfo = [];
 
 const List = (probs) => {
-  const [cards,setCards] = useState(card_info);
+  const [cards, setCards] = useState(cardInfo);
   const clickHandler = (cards) => {
     setCards((prevLists) => {
-      return [cards, ...prevLists]
+      return [cards, ...prevLists];
     });
   };
   return (
     <div className="list">
       <div className="header">
-        <p className="header-title">
-          اسم
-        </p>
+        <p className="header-title">اسم</p>
         <button className="header-button">
-          <p className="button-title">
-            ...
-          </p>
+          <p className="button-title">...</p>
         </button>
       </div>
       <div className="card-list">
-        {cards.map(() => <Card/>)}
+        {cards.map(() => (
+          <Card />
+        ))}
       </div>
       {/* <div className="space"></div> */}
       <div className="add-card">
-        <button className="add-card-button" onClick={clickHandler}>+ افزودن کارت</button>
+        <button className="add-card-button" onClick={clickHandler}>
+          + افزودن کارت
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default List;
