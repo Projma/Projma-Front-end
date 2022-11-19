4d79f7dcb1588714cdf6af970cf5a67f33d70b67
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2022-11-19 15:26:37 +0330

    [feat] workspace page ready for different tabs

--- src/components/Workspace_management/Members/Members.jsx
+++ src/components/Workspace_management/Members/Members.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "./Members.css";
 const Members = () => {
-  return <div>salam</div>;
+  return (
+    <div className="main-div">
+      {/* <h1 style={{ color: "white", backgroundColor: "white" }}>Members</h1> */}
+    </div>
+  );
 };
 
 export default Members;
--- src/pages/Workspace_management.jsx
+++ src/pages/Workspace_management.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ResponsiveDrawerRight from "../components/Workspace_management/Drawer/Drawer";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Members from "../components/Workspace_management/Members/Members";
+import "../styles/Workspace_management.css";
+
 const Workspace_management = () => {
-  return <ResponsiveDrawerRight />;
+  return (
+    <div className="mother-div">
+      {/* <div> */}
+      <ResponsiveDrawerRight width={"249px"} />
+      {/* </div> */}
+      {/* <Link to="workspace/members">Members</Link> */}
+      {/* <h1 style={{ color: "white", backgroundColor: "white" }}>Members</h1> */}
+      <Routes>
+        <Route path="members" element={<Members />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Workspace_management;
