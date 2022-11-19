e8c9d8fd378f244e696ba26044de957cd9715d96
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2022-11-19 15:25:44 +0330

    [fix] drawer more responsive and new styles

--- src/components/Workspace_management/Drawer/Drawer.jsx
+++ src/components/Workspace_management/Drawer/Drawer.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -7,6 +8,9 @@ import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
+import PeopleIcon from "@mui/icons-material/People";
+import SettingsIcon from "@mui/icons-material/Settings";
+import BoardIcon from "@mui/icons-material/Assignment";
 import { makeStyles } from "@mui/styles";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -17,10 +21,12 @@ import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import DehazeIcon from "@mui/icons-material/Dehaze";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import Members from "../Members/Members";
 import "./Drawer.css";
 
 const drawerWidth = 250;
@@ -28,8 +34,18 @@ const drawerWidth = 250;
 const useStyles = makeStyles({
   close_drawer: {
     "&:hover": {
-      color: "white",
+      backgroundColor: "#B0ACB1",
     },
+  },
+  list_item: {
+    "&:hover": {
+      backgroundColor: "#223040",
+    },
+    fontFamily: "Vazir",
+  },
+  list_item_icon: {
+    color: "#fff",
+    borderRadius: "50%",
   },
 });
 
@@ -50,7 +66,7 @@ function ResponsiveDrawer(props) {
     <div id="right-arrow-div">
       <KeyboardArrowRightIcon
         className={classes.close_drawer}
-        sx={{ fontSize: "30px" }}
+        sx={{ fontSize: "30px", backgroundColor: "white" }}
         onClick={handleDrawerToggle}
       />
     </div>
@@ -61,33 +77,100 @@ function ResponsiveDrawer(props) {
       {matches ? "" : right_arrow}
       <Divider />
       <List>
-        {["Boards", "Members", "settings"].map((text, index) => (
+        {["بورد‌ها", "اعضا", "تنظیمات"].map((text, index) => (
           <ListItem
+            button
+            href="#"
             key={text}
             disablePadding
-            sx={{
-              backgroundColor: "#0A1929",
-            }}
+            className={classes.list_item}
+            sx={
+              {
+                // backgroundColor: "#0A1929",
+              }
+            }
           >
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} sx={{ color: "#fff" }} />
-            </ListItemButton>
+            <Link
+              to="members"
+              className={classes.list_item}
+              style={{
+                textDecoration: "none",
+                color: "#000",
+                display: "block",
+                width: "100%",
+              }}
+            >
+              <ListItemButton href="/members" sx={{}}>
+                <ListItemIcon>
+                  {index === 0 ? (
+                    <BoardIcon className={classes.list_item_icon} />
+                  ) : index === 1 ? (
+                    <PeopleIcon className={classes.list_item_icon} />
+                  ) : (
+                    <SettingsIcon className={classes.list_item_icon} />
+                  )}
+                </ListItemIcon>
+                <ListItemText
+                  disableTypography
+                  primary={
+                    <Typography
+                      type="body2"
+                      style={{ color: "#fff", fontFamily: "Vazir" }}
+                    >
+                      {text}
+                    </Typography>
+                  }
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    fontSize: "1.3rem",
+                  }}
+                />
+              </ListItemButton>
+            </Link>
           </ListItem>
         ))}
       </List>
-      <Divider />
+      <Divider sx={{ backgroundColor: "#9499a8" }} />
       <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
+        {["همه میل‌ها", "زباله", "اسپم"].map((text, index) => (
+          <ListItem key={text} className={classes.list_item} disablePadding>
+            <Link
+              to="/members"
+              style={{
+                textDecoration: "none",
+                color: "#000",
+                display: "block",
+                width: "100%",
+              }}
+            >
+              <ListItemButton sx={{ textDecoration: "none" }}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? (
+                    <InboxIcon className={classes.list_item_icon} />
+                  ) : (
+                    <MailIcon className={classes.list_item_icon} />
+                  )}
+                </ListItemIcon>
+                <ListItemText
+                  disableTypography
+                  primary={
+                    <Typography
+                      type="body2"
+                      style={{ color: "#fff", fontFamily: "Vazir" }}
+                    >
+                      {text}
+                    </Typography>
+                  }
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    fontSize: "1.3rem",
+                    color: "#fff",
+                  }}
+                />
+              </ListItemButton>
+            </Link>
           </ListItem>
         ))}
       </List>
@@ -98,81 +181,75 @@ function ResponsiveDrawer(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      {/* <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      > */}
-      {/* <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
-        </Toolbar> */}
-      {/* </AppBar> */}
-      <KeyboardArrowLeftIcon
+    <div style={{ height: "5%" }}>
+      <DehazeIcon
         onClick={handleDrawerToggle}
         sx={{
-          fontSize: "40px",
+          fontSize: "3rem",
+          // backgroundColor: "#B0ACB1",
+          // backgroundColor: "#6d88e3",
+          color: "#B0ACB1",
+          borderRadius: "50%",
+          marginTop: "10px",
+          marginRight: "10px",
         }}
       />
-      <Box
-        component="nav"
-        sx={{
-          width: { sm: drawerWidth },
-          flexShrink: { sm: 0 },
-        }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          anchor="right"
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
+      <div className="main-container" style={{ width: props.width }}>
+        <Box
           sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-              // backgroundColor: "#1c334e",
-            },
+            display: "flex",
+            width: props.width,
+            // display: mobileOpen ? "none" : "block",
+            // ...(mobileOpen === true && { display: "none" }),
           }}
         >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          anchor="right"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
+          <CssBaseline />
+          <Box
+            component="nav"
+            sx={{
+              width: { sm: drawerWidth },
+              width: "100%",
+              flexShrink: { sm: 0 },
+            }}
+            aria-label="mailbox folders"
+          >
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Drawer
+              container={container}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              anchor="right"
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              sx={{
+                display: { xs: "block", sm: "none" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                  // backgroundColor: "#1c334e",
+                },
+              }}
+            >
+              {drawer}
+            </Drawer>
+            <Drawer
+              variant="permanent"
+              anchor="right"
+              sx={{
+                display: { xs: "none", sm: "block" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Box>
+          {/* <Box
         component="main"
         sx={{
           flexGrow: 1,
@@ -180,38 +257,10 @@ function ResponsiveDrawer(props) {
           width: { sm: `calc(100% - ${drawerWidth}px)` },
         }}
       >
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
-    </Box>
+      </Box> */}
+        </Box>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Workspace_management/Members/Members.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Members = () => {
+  return <div>salam</div>;
+};
+
+export default Members;
