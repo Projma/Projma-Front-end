ead537bec8d97d8653b50d55adfec88d4b020cf1
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2022-12-01 21:53:40 +0330

    [refactor]: new header in landing

--- src/components/Landing/Nav/Nav.jsx
+++ src/components/Landing/Nav/Nav.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Nav.css";
 import ResponsiveAppBar from "../ResponsiveAppBar/ResponsiveAppBar";
+// import Header from "../../Header/Header";
 const Nav = () => {
   // return <nav>here is the nav</nav>;
   return <ResponsiveAppBar />;
+  // return <Header />;
 };
 
 export default Nav;
