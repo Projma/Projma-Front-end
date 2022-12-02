af59c724e8ed1db612c3c2558eb2a4f545269453
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2022-12-02 21:52:00 +0330

    [feat]: cutomize basic menu mui

--- src/components/ResponsiveAppBar/BasicMenu/BasicMenu.jsx
+++ src/components/ResponsiveAppBar/BasicMenu/BasicMenu.jsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
+// import Typography from '@mui/material/Typography';
+import BasicModal from '../../Dashboard/Modal/BasicModal';
 
 export default function BasicMenu(props) {
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -12,6 +15,12 @@ export default function BasicMenu(props) {
     const handleClose = () => {
         setAnchorEl(null);
     };
+
+    const navigate = useNavigate();
+    const navigateToWorkspace = (workspaceId) => {
+        handleClose();
+        navigate(`/workspace/${workspaceId}`);
+    }
 
     return (
         <div>
@@ -35,9 +44,50 @@ export default function BasicMenu(props) {
                     'aria-labelledby': 'basic-button',
                 }}
             >
-                {props.items.map((item) => (
+                {/* {props.items.map((item) => (
+                    {item}
+                ))} */}
+                {/* {props.items.map((item) => (
                     <MenuItem onClick={handleClose} key={item}>{item}</MenuItem>
-                ))}
+                ))} */}
+                {/* {
+                    "2": "تست 1",
+                    "3": "تست 2",
+                    "4": "تست 3",
+                    "5": "تست 6",
+                    "6": "تست 6",
+                    "7": "تست",
+                    "8": "لی",
+                    "20": "تست 10ddfv"
+                } */}
+                {/* {console.log(props.items)}  */}
+                {/* {console.log("| up |")} */}
+                {
+                    (props.name == 'فضای کارها') && (Object.keys(props.workspaces).map((key) => {
+                        return (
+                            <MenuItem onClick={() => navigateToWorkspace(key)} key={key} sx={{
+                                ":hover": {
+                                    color: '#E2EDF8',
+                                    backgroundColor: '#007fff',
+                                    borderRadius: '5px',
+                                }
+                            }}>
+                                {props.workspaces[key]}
+                            </MenuItem>
+                        )
+                    }))
+                }
+                {
+                    (props.name == 'ایجاد') && (<MenuItem sx={{
+                                ":hover": {
+                                    color: '#E2EDF8',
+                                    backgroundColor: '#007fff',
+                                    borderRadius: '5px',
+                                }
+                            }}>
+                        <BasicModal text="ایجاد فضای کاری جدید" />
+                    </MenuItem>)
+                }
                 {/* <MenuItem onClick={handleClose}>Profile</MenuItem>
                 <MenuItem onClick={handleClose}>My account</MenuItem>
                 <MenuItem onClick={handleClose}>Logout</MenuItem> */}
