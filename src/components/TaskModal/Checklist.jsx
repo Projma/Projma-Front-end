15606bf2761998b71e2181fd4964f626d4e93fb8
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2022-12-07 11:39:26 +0330

    [chore] checklist configuration in taskmodal is done

---
+++ src/components/TaskModal/Checklist.jsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import LabelIcon from "@mui/icons-material/Label";
+import Divider from "@mui/material/Divider";
+import "../../styles/TaskModal.css";
+import "./Checklist.scss";
+
+export default function CheckList() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  return (
+    <div>
+      <Button
+        className="taskmodal-smaller-button-inner"
+        aria-describedby={id}
+        variant="contained"
+        onClick={handleClick}
+        sx={{
+          bgcolor: "#173b5e",
+          marginTop: "5%",
+        }}
+      >
+        <LabelIcon rotate="90" fontSize="large"></LabelIcon>{" "}
+        <div className="taskmodal-smaller-button">لیبل</div>
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        hi
+      </Popover>
+    </div>
+  );
+}
