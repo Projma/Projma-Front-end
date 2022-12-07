18f43f21d19c705c064b8ee1d210b76fed7b9b93
VahidM313 <vahidood@gmail.com>
2022-12-07 19:14:54 +0330

    [feat]: add kanban component and style

---
+++ src/components/Kanban/Kanban.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import Board from "../Board/UI/Board";
+
+const Kanban = () => {
+  return (
+    <div className="Kanban_main-page">
+      <Header/>
+      <Board/>
+    </div>
+  );
+};
+
+export default Kanban;
