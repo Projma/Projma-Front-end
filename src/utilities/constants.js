c3ddbe22391a4838821a81a79f11d82df686f890
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2022-12-08 16:32:48 +0330

    [feature] adding label to board enabled but has bugs

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,4 +2,4 @@
 // import createBrowserHistory from "history/createBrowserHistory";
 // export const history = createBrowserHistory();
 export const swaggerUrl = "http://mohammadosoolian.pythonanywhere.com/swagger/";
-export const baseUrl = "http://mohammadosoolian.pythonanywhere.com/";
+export const baseUrl = "http://127.0.0.1:8000/";
