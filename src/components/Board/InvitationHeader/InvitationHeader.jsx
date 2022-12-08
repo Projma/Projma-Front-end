e456fe3192d974f89fd8ee7bce702736946caecf
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2022-12-08 14:39:53 +0330

    [feat]: invitation avatar

---
+++ src/components/Board/InvitationHeader/InvitationHeader.jsx
@@ -0,0 +1,14 @@
+// rface snippet
+import React from 'react';
+import "./InvitationHeader.scss";
+import GroupAvatars from '../GroupAvatars/GroupAvatars';
+
+const InvitationHeader = () => {
+    return (
+        <div>
+            <GroupAvatars/>
+        </div>
+    )
+}
+
+export default InvitationHeader
