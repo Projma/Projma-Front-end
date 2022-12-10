ce92df56d5216b161d987fd1b55ea6acabe4b256
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2022-12-10 12:29:54 +0330

    [fix]: pv route clean code

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,5 @@
 import * as React from 'react';
-// import Button from '@mui/material/Button';
-// import Menu from '@mui/material/Menu';
-// import MenuItem from '@mui/material/MenuItem';
 import { useNavigate } from 'react-router-dom';
-// import Typography from '@mui/material/Typography';
-// import BasicModal from '../../Dashboard/Modal/BasicModal';
-// import { color } from '@mui/system';
-// // import BasicModal from "../components/Workspace_management/BasicModal/CreateBoard"; create board modal
-// // import { BasicModal as CreateBoard } from '../../Workspace_management/BasicModal/CreateBoard';
 // import apiInstance from '../../../utilities/axiosConfig';
 // import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
