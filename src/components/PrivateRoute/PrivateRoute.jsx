528c3066f08f663b724b3a09b55b292a6f8439f6
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2022-12-10 12:27:58 +0330

    [feat]: pv route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -11,19 +11,28 @@ import { useNavigate } from 'react-router-dom';
 // import apiInstance from '../../../utilities/axiosConfig';
 // import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 export default function PrivateRoute(props) {
     const state = useSelector((state) => state);
     const navigate = useNavigate();
-    if (state.auth.isAuthenticated) {
+    useEffect(() => {
+        console.log("isAuthenticated", state.isAuthenticated);
+        if (! state.isAuthenticated) {
+            navigate('/signin/');
+        }
+    }, [state.isAuthenticated]);
+
+    if (state.isAuthenticated) {
         return <>
             {props.children}
         </>;
     }
     else {
-        navigate('/signin');
-        return (
-            <></>
-        );
+        // navigate('/signin');
+        // navigateSignIn();
+        // return (
+        //     <></>
+        // );
     }
 }
