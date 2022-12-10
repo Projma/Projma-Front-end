11cc309f0207dfc4f6363430a1637bce5b39b3bd
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2022-12-10 12:39:14 +0330

    [feat]: add pv routes

--- src/App.js
+++ src/App.js
@@ -45,16 +45,15 @@ function App() {
         <Router>
           <Routes>
             <Route exact path="/" element={<Landing />} />
-            {/* <Route exact path="/dashboard" element={<Dashborad />} /> */}
             <Route exact path="/dashboard" element={<PrivateRoute children={<Dashborad/>}/>} />
             <Route exact path="/signin" element={<SignIn />} />
             <Route exact path="/signup" element={<SignUp />} />
-            <Route exact path="/profile" element={<Profile />} />
+            <Route exact path="/profile" element={<PrivateRoute children={<Profile/>}/>} />
             <Route exact path="/forget-password" element={<ForgetPassword />} />
             <Route exact path="/reset-password" element={<ResetPassword />} />
             <Route path="workspace/:id/*" element={<Workspace_management />} />
             <Route exact path="/profileview/" element={<ProfileView />} />
-            <Route exact path="/changepassword" element={<ChangePassword />} />
+            <Route exact path="/changepassword" element={<PrivateRoute children={<ChangePassword/>}/>} />
             <Route exact path="/kanban/:id" element={<Kanban />} />
             <Route exact path="/invite_page/:token" element={<InvitePage />} />
             <Route
