70eeb59ce1a2b791823c875eff0840e180c372c0
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2022-12-13 20:13:04 +0330

    [fix]: solving problem of not showing info in editmodal

--- src/components/Workspace_management/Navbar/Navbar.js
+++ src/components/Workspace_management/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import EditModal from "../WorkspaceEditModal/EditModal";
 import apiInstance from "../../../utilities/axiosConfig";
+import "transition-style";
 import "./Navbar.css";
 const Navbar = ({ params }) => {
   const [workspace, setWorkspace] = React.useState({});
+  const [name, setName] = React.useState("");
   useEffect(() => {
     apiInstance
       .get(`workspaces/workspaceowner/${params.id}/get-workspace/`)
@@ -13,6 +15,7 @@ const Navbar = ({ params }) => {
           "*********************************************************"
         );
         setWorkspace(res.data);
+        setName(res.data.name);
         console.log(workspace);
       })
       .catch((err) => {
@@ -20,13 +23,25 @@ const Navbar = ({ params }) => {
       });
   }, []);
   // console.log("in nav bar", workspace);
+  const update_navbar = (data) => {
+    console.log("god damn");
+    console.log(data);
+    console.log(data.name);
+    setWorkspace(data);
+    setName(data.name);
+  };
 
   return (
     <div className="ws_navbar-main-div">
-      <div className="ws_navbar-symbol">{workspace.name}</div>
+      <div className="ws_navbar-ws-name">
+        <div transition-style className="ws_navbar-symbol --in-custom">
+          <h3>{name?.charAt(0).toUpperCase()}</h3>
+        </div>
+        <div className="ws_navbar-ws-name-text">{workspace.name}</div>
+      </div>
       <div className="ws_navbar-edit">
         <button className="ws_navbar-edit-button">
-          <EditModal params={params} />
+          <EditModal params={params} update_navbar={update_navbar} />
         </button>
       </div>
     </div>
