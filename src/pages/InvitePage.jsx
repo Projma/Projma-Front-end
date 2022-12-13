ccff0170a532a67ce5158a4b8f3160a045b02550
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2022-12-13 20:16:31 +0330

    [feat]: add invite page

--- src/pages/InvitePage.jsx
+++ src/pages/InvitePage.jsx
@@ -1,47 +1,66 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
 import apiInstance from "../utilities/axiosConfig";
-import { Helmet } from "react-helmet";
 
 const InvitePage = () => {
+  const navigate = useNavigate();
   const params = useParams();
+  const [error, setError] = useState("");
+  const [errorRes, setErrorRes] = useState("");
+  const [result, setResult] = useState("");
+  const [progress, setProgress] = useState(0);
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   useEffect(() => {
     console.log(params.token);
-    apiInstance
-      .get(`workspaces/workspaces/join-to-workspace/${params.token}/`)
-      .then((res) => {
-        if (res.status === 200) {
+    try {
+      apiInstance
+        .get(`workspaces/workspaces/join-to-workspace/${params.token}/`)
+        .then((res) => {
+          console.log("there");
+          console.log(res.data);
           setResult(success);
-        } else {
-          // setError(res.data);
-          setResult(failure);
-          console.log("in else");
-        }
-        if (res.status === 400) {
-          console.log("here");
-        }
-        console.log(res);
-        console.log(res.data);
-      });
+          delay(4000).then(() => navigate("/dashboard"));
+        });
+    } catch (error) {
+      console.log("here");
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        setError(error.response.data);
+        setErrorRes(error.response.status);
+        setResult(failure);
+      }
+    }
   }, []);
-  const [result, setResult] = useState(null);
-  const [error, setError] = useState("");
   const success = (
-    <div>
-      <h1 style={{ color: "#fff" }}>Success</h1>
+    <div className="invite_page-success invite_page-main-div">
+      <h1 style={{ fontSize: "5rem" }}>😄</h1>
+      <h1 style={{ color: "#fff", marginBottom: "1rem" }}>
+        با موفقیت به فضای کار اضافه شدید!
+      </h1>
+      <p style={{ marginBottom: "1rem" }}>در حال انتقال به صفحه داشبورد</p>
+      <Box sx={{ width: "100%", color: "#fff" }}>
+        <LinearProgress color="success" />
+      </Box>
     </div>
   );
   const failure = (
-    <div>
-      <h1 style={{ color: "#fff" }}>Failure</h1>
+    <div className="invite_page-failure invite_page-main-div">
+      <h1 style={{ fontSize: "5rem" }}>😔</h1>
+      <h1 style={{ color: "#fff", marginBottom: "1rem" }}>
+        متاسفانه به فضای کار اضافه نشدید!
+      </h1>
+      <h2 style={{ color: "#000", marginBottom: "1rem" }}> متن خطا:</h2>
+      <h3 style={{ color: "#fff", fontFamily: "sans-serif" }}>{error}</h3>
     </div>
   );
   return (
     <div style={{ backgroundColor: "white", width: "100%", height: "100%" }}>
-      <Helmet>
-        <title>صفحه دعوت</title>
-      </Helmet>
       {result}
     </div>
   );
