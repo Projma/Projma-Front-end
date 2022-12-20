36eb664241052c1e9391878fcdb346e779fcfdc3
Navid <navid8020@gmail.com>
2022-12-20 19:16:35 +0330

    [fix]: fix attachment box

--- src/components/TaskModal/TaskModal.jsx
+++ src/components/TaskModal/TaskModal.jsx
@@ -222,6 +222,7 @@ export default function TaskModal() {
   const [changePlus, setChangePlus] = useState(false);
   const [dueDate, setDueDate] = useState("");
   const [taskdoner, setTaskDoner] = useState("");
+  const [allAttachments, setAllAttachments] = useState([]);
   const [EditCheckList, setEditCheckList] = useState(Array(1000).fill(false));
   const [EditCommentList, setEditCommentList] = useState(
     Array(1000).fill(false)
@@ -229,6 +230,16 @@ export default function TaskModal() {
   const [title, setTitle] = useState("");
   const baseURL = baseUrl.substring(0, baseUrl.length - 1);
   // const params = useParams();
+  const handleRemoveAttachment = (id) => {
+    setAllAttachments((prevState) => {
+      return prevState.filter((item) => item.id !== id);
+    });
+    apiInstance
+      .delete(`/workspaces/attachment/${id}/delete-attachment-from-task/`)
+      .then((res) => {
+        console.log(res);
+      });
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     setShowDescription(false);
@@ -397,6 +408,12 @@ export default function TaskModal() {
         setDone(res.data.spend);
         setDescription(res.data.description);
         setTitle(res.data.title);
+        const attachments = res.data.attachments.map((obj) => ({
+          id: obj.id,
+          file: obj.file,
+          created_at: obj.created_at,
+        }));
+        setAllAttachments(attachments);
         const labels = res.data.labels.map((item) => ({
           id: item.id,
           title: item.title,
@@ -852,6 +869,72 @@ export default function TaskModal() {
                       </Box>
                     </div>
                   </div>
+
+                  <div className="taskmodal-body-attachment">
+                    <div className="flex-row taskmodal-body-attachment-header">
+                      <div className="flex-taskmodal taskmodal-body-attachment-icon">
+                        <AttachFileIcon
+                          fontSize="large"
+                          sx={{ color: "white" }}
+                        ></AttachFileIcon>
+                      </div>
+                      <div className="flex neonText taskmodal-description-title">
+                        پیوست
+                      </div>
+                    </div>
+                    <div>
+                      <div className="taskmodal-body-attachment-list">
+                        {allAttachments.map((item, i) => (
+                          <div
+                            style={{
+                              display: "flex",
+                              flexDirection: "row",
+                              justifyContent: "space-between",
+                            }}
+                          >
+                            <Button
+                              // component={Link}
+                              href={`${item.file}`}
+                              className="taskmodal-attachment-link-btn"
+                            >
+                              <div className="flex-taskmodal taskmodal-body-attachment-list-item-icon">
+                                <AttachFileIcon
+                                  sx={{ color: "white", fontSize: "46px" }}
+                                ></AttachFileIcon>
+                              </div>
+                              <div className="flex-row taskmodal-body-attachment-list-item">
+                                <div className="flex-taskmodal taskmodal-body-attachment-list-item-title">
+                                  {item.file.toString().split("/")[5]}
+                                </div>
+                                <div className="flex-row">
+                                  <div className="flex-taskmodal taskmodal-body-attachment-list-item-createdTime">
+                                    <div style={{ width: "72px" }}>
+                                      {item.created_at}
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </Button>
+                            <Button
+                              onClick={() => handleRemoveAttachment(item.id)}
+                              sx={{
+                                fontFamily: "Vazir",
+                                fontSize: "10px",
+                                display: "flex",
+                                paddingTop: "0px",
+                                marginLeft: "2%",
+                                justifyContent: "center",
+                                color: "white",
+                              }}
+                            >
+                              حذف
+                            </Button>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+
                   <div className="taskmodal-body-activity">
                     <div className="flex-row taskmodal-body-activity-header">
                       <div className="flex-taskmodal taskmodal-body-activity-icon">
