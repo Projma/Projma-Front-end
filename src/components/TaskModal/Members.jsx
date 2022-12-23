d0e1c9a9dd1456dc7bff1755a3215c43eff3c44c
Navid <navid8020@gmail.com>
2022-12-23 21:10:31 +0330

    [fix]: fix checkbox error

--- src/components/TaskModal/Members.jsx
+++ src/components/TaskModal/Members.jsx
@@ -75,7 +75,6 @@ export default function Members({ params, setDoers, doer }) {
     if (check_username_in_list(member.userName, doer)) {
       setDoers(doer.filter((item) => item.userName !== member.userName));
       console.log("if");
-      formData.append("doers", [member.id]);
       apiInstance
         .patch(
           `/workspaces/task/${params.task_id}/delete-doers-from-task/`,
@@ -87,7 +86,7 @@ export default function Members({ params, setDoers, doer }) {
         });
     } else {
       setDoers([...doer, member]);
-      formData.append("doers", [member.id]);
+      const form_data = { doers: [member.id] };
 
       apiInstance
         .patch(`/workspaces/task/${params.task_id}/add-doers-to-task/`, json)
