1e7f40b5d260b74976cb1a4c178520f93152ac35
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2022-12-27 20:05:16 +0330

    [feat]: starred boards computer

--- src/pages/Dashborad.jsx
+++ src/pages/Dashborad.jsx
@@ -49,6 +49,7 @@ export const Dashborad = () => {
   let [owningWorkspaces, setOwningWorkspaces] = useState([]);
   let [boards, setBoards] = useState([]);
   let [recentBoards, setRecentBoards] = useState([]);
+  let [starredBoards, setStarredBoards] = useState([]);
   let flag = 0;
 
   useEffect(() => {
@@ -135,6 +136,19 @@ export const Dashborad = () => {
         setRecentBoards(response.data);
         // console.log("recent", response.data);
       })
+      .catch((error) => {
+        // console.log(error);
+      });
+
+    apiInstance
+      .get("/workspaces/dashboard/mystarred-boards/")
+      .then((response) => {
+        setStarredBoards(response.data);
+        // console.log("starred", response.data);
+      })
+      .catch((error) => {
+        // console.log(error);
+      });
   }, [flag]);
 
   const [boardsInfo, setBoardsInfo] = useState([]);
@@ -169,6 +183,76 @@ export const Dashborad = () => {
           <p variant="h1" component="h2" className="text paragraph">
             <StarPurple500TwoToneIcon sx={{ ml: 1.5 }} /> بورد های ستاره‌دار
           </p>
+          <Grid
+            container
+            columns={{ xs: 2, sm: 4, md: 8 }}
+            // spacing={{ xs: 1, sm: 2, md: 3 }}
+            sx={{
+              // paddingTop: "5%",
+              // marginTop: "10%",
+              marginBottom: "7%",
+              // backgroundColor: "#f5f5f5",
+            }}
+          >
+            {starredBoards.map((board) => {
+              return (
+                <Grid item xs={2} sm={2} md={2} key={board["id"]} sx={{}}>
+                  <Paper
+                    sx={{
+                      padding: "3%",
+                      textAlign: "center",
+                      // color: "#007fff",
+                      backgroundColor: "#007fff", // 5090D3
+                      borderRadius: "10px",
+                      // width: "100%",
+                      // height: "100%",
+                      // minWidth: "200px",
+                      // maxWidth: "300px",
+                      minHeight: "150px",
+                      // maxHeight: "300px",
+                      margin: "10%",
+                      // padding: "10px",
+                      // display: "flex",
+                      // justifyContent: "center",
+                      // alignItems: "center",
+                      // flexDirection: "column",
+                      ":hover": {
+                        backgroundColor: "#5090D3",
+                        cursor: "pointer",
+                      },
+                    }}
+                    // hover
+                    onClick={() => {
+                      // history.push(`/board/${board_id}`);
+                      navigateToBoard(board["id"]);
+                    }}
+                  >
+                    {/* `/workspaces/boardsmemberapi/${board_id}/get-board/` */}
+                    <p
+                      variant="h1"
+                      component="h2"
+                      className="text paragraph"
+                    >
+                      {/* check that is null or not */}
+                      {board["name"]
+                        ? board["name"]
+                        : "بی‌نام"}
+                    </p>
+                    <p
+                      variant="h1"
+                      component="h2"
+                      className="text paragraph"
+                    >
+                      {board["description"]
+                        ? board["description"]
+                        : "بدون توضیحات"}
+                    </p>
+                    {/* </> */}
+                  </Paper>
+                </Grid>
+              );
+            })}
+          </Grid>
           <Divider sx={{ bgcolor: "#007fff", marginTop: "5%" }} />
           <p variant="h1" component="h2" className="text paragraph">
             <AvTimerTwoToneIcon sx={{ ml: 1.5 }} /> اخیرا دیده شده
