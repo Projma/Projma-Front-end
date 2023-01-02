b172b8786e528e448b4bf745acded81e6a4e0f98
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2023-01-02 21:26:43 +0330

    [feat}: add not dound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,19 +1,86 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import "../styles/NotFound.scss";
+import Header from "../components/Header/Header";
+import Footer from "../components/Landing/Footer/Footer";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid"; // Grid version 1
+import Container from "@mui/material/Container";
+import { Box } from "@mui/system";
+import { Divider } from "@mui/material";
+import page_not_found from "../static/images/not found/page_not_found.svg"
 
 
-const Landing = () => {
+const NotFound = () => {
     return (
         <div>
             <Helmet>
                 <title>پروجما</title>
             </Helmet>
-            <p>
-                صفحه مورد نظر یافت نشد
-            </p>
+            <Header />
+
+            <Container maxWidth="lg" sx={{ marginTop: "2rem" }}>
+                <Grid container xs={12} md={12} lg={12} >
+                    <Grid item xs={12} md={12} lg={12} 
+                    // sx={{
+                    //     display: "flex",
+                    //     justifyContent: "center",
+                    //     alignItems: "center",
+                    // }}
+                    >
+                        <img src={page_not_found} className="top-img" />
+                        <Divider sx={{ bgcolor: "#007fff", marginTop: "5%" }} />
+                    </Grid>
+                    <Grid item xs={12} md={12} lg={12} sx={{
+                        // marginTop: "5%",
+                    }}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: "flex",
+                                flexDirection: "column",
+                                height: 240,
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                }}
+                            >
+                                <h1>صفحه مورد نظر یافت نشد</h1>
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                }}
+                            >
+                                <h1>ارور 404</h1>
+                            </Box>
+                            <Divider />
+                            <Box sx={{ pt: 1 }}>
+                                <p>
+                                    صفحه مورد نظر شما یافت نشد. لطفا آدرس صفحه را
+                                    بررسی کنید.
+                                </p>
+                                {/* <img src={page_not_found} className="top-img" /> */}
+                            </Box>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+
+
+            <Footer
+            // sx={{
+            //     position: "absolute",
+            //     bottom: 0,
+            //     width: "100%",
+            // }}
+            />
         </div>
     );
 };
 
-export default Landing;
+export default NotFound;
