24b2318b88ae8e96a15048408f4d2c0d0581d92f
VahidM313 <vahidood@gmail.com>
2023-03-13 00:12:23 +0330

    chore: change backend constants to local

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,9 +1,9 @@
 //https://stackoverflow.com/questions/48514773/use-history-push-in-action-creator-with-react-router-v4
 // import createBrowserHistory from "history/createBrowserHistory";
 // export const history = createBrowserHistory();
-// export const swaggerUrl = "http://127.0.0.1:8000/swagger/";
-// export const baseUrl = "http://127.0.0.1:8000/";
-export const swaggerUrl = "http://mohammadosoolian.pythonanywhere.com/swagger/";
-export const baseUrl = "http://mohammadosoolian.pythonanywhere.com/";
+export const swaggerUrl = "http://127.0.0.1:8000/swagger/";
+export const baseUrl = "http://127.0.0.1:8000/";
+// export const swaggerUrl = "http://mohammadosoolian.pythonanywhere.com/swagger/";
+// export const baseUrl = "http://mohammadosoolian.pythonanywhere.com/";
 // export const swaggerUrl = "http://37.32.27.51/swagger/";
 // export const baseUrl = "http://37.32.27.51/";
