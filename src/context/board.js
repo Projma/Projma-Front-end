c97dc8411f2f77cf8be8107dde28b30e948661e0
VahidM313 <vahidood@gmail.com>
2023-03-13 00:11:42 +0330

    feat: add card to list and remove list func

--- src/context/board.js
+++ src/context/board.js
@@ -1,11 +1,12 @@
 import React, { createContext, useState, useCallback } from "react";
-import apiInstance from "../../utilities/axiosConfig";
+import apiInstance from "../utilities/axiosConfig";
 import Loading from "../components/Shared/Loading";
 
 const BoardContext = createContext();
 
 function Provider({ children, boardId }) {
   const [list, setList] = useState([]);
+  const [member, setMember] = useState([])
   const [isReq, setIsReq] = useState(false);
 
   const getBoard = useCallback(async () => {
@@ -14,15 +15,37 @@ function Provider({ children, boardId }) {
       .get(`workspaces/board/${boardId}/get-board-overview/`)
       .then((response) => {
         setList(response.data.tasklists.sort((a, b) => b.order - a.order));
+        setMember(response.data.members);
       })
       .finally(() => setIsReq(false));
   }, [boardId]);
 
+  const addCardToList = (card, list_id) => {
+    setList(
+      list.map((list) => {
+        if (list.id === list_id) {
+          return {
+            ...list,
+            tasks: [...list.tasks, card],
+          };
+        }
+        return list;
+      })
+    );
+  };
+
+  const removeList = (id) => {
+    setList(list.filter((list) => list.id !== id));
+  };
+
   const board = {
     boardId,
     list,
+    member,
     setList,
     getBoard,
+    addCardToList,
+    removeList,
     setIsReq,
   };
 
