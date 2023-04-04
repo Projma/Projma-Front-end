11215e794808ef63ac0d2e0569bf63cbc8e05aa7
Navid <navid8020@gmail.com>
2023-04-05 00:36:26 +0330

    [chore]: clean

--- src/components/Registration/__test__/Signup.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import SignUp from "../Signup";
-
-it("renders learn react link", () => {
-  render(<SignUp />);
-  const linkElement = screen.getByPlaceholderText(/نام/i);
-  expect(linkElement).toBeInTheDocument();
-});
