000501903d27314027f7562697763c5cec728428
Navid <navid8020@gmail.com>
2023-04-05 12:17:40 +0330

    [fix]: add comments for tests

--- src/components/Registration/Signin.jsx
+++ src/components/Registration/Signin.jsx
@@ -18,7 +18,7 @@ import StyledTextField from "./StyledTextField";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import apiInstance from "../../utilities/axiosConfig";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; // comment for tests
 import { useDispatch } from "react-redux";
 import { login } from "../../actions/authActions";
 import { useState } from "react";
@@ -51,7 +51,7 @@ export default function SignIn() {
   const [isPost, setIsPost] = useState(false);
 
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  let navigate = useNavigate();
+  let navigate = useNavigate(); // comment for tests
   // const dispatch = useDispatch();
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -73,7 +73,7 @@ export default function SignIn() {
           rtl: true,
         });
         // dispatch(login());
-        delay(4000).then(() => navigate("/dashboard"));
+        delay(4000).then(() => navigate("/dashboard")); // comment for tests
       })
       .catch((res) => {
         if (res.request.response.search("active") !== -1) {
