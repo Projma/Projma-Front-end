5a0b2baac5c0b886a614f56a0f4ce18de5153dc8
Vahid Mohammdi <vahidood@gmail.com>
2023-04-20 22:58:25 +0330

    feat: add poll view component

--- src/components/Poll/PollView.js
+++ src/components/Poll/PollView.js
@@ -1,5 +1,32 @@
+import {Typography} from "@mui/material";
+import HowToVoteIcon from '@mui/icons-material/HowToVote';
+import "./PollView.css";
+import React, {useEffect, useState} from "react";
+import AnonymousVoter from "./Content/AnonymousVoter";
+import SingleVote from "./Content/SingleVote";
+
 const PollView = () => {
-  return ( <></> );
+  const data = {question: "Are you Gay",options: ["Yes","No"]};
+  const [tVote, setTVote] = useState(0);
+  useEffect(() => {
+  }, [tVote]);
+  return (
+    <div className="poll_pollview-container">
+      <div className="poll_pollview-label">
+        <Typography>{data.question}</Typography>
+      </div>
+      <div className="poll_pollview-attendents">
+        <AnonymousVoter/>
+      </div>
+      <div className="poll_pollview-options">
+        <SingleVote op={data.options} setTVote={setTVote}/>
+      </div>
+      <div className="poll_pollview-results">
+        <Typography fontSize="1.1rem">{tVote}</Typography>
+        <HowToVoteIcon sx={{width:"1.5rem",height:"1.5rem"}}/>
+      </div>
+    </div>
+  );
 }
- 
+
 export default PollView;
