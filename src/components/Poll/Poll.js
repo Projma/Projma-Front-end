ad6002550db20cb982ba338034be5abd908ee62c
Vahid Mohammdi <vahidood@gmail.com>
2023-04-21 22:46:10 +0330

    feat: add setting popover

--- src/components/Poll/Poll.js
+++ src/components/Poll/Poll.js
@@ -1,16 +1,20 @@
 import "./Poll.css";
 import AddPoll from "./AddPoll/AddPoll";
 import PollView from "./PollView";
-import { Fab, Modal } from "@mui/material";
+import {Button, Fab, Modal, Popover} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import apiInstance from "../../utilities/axiosConfig";
 import useBoard from "../../hooks/useBoard";
+import {DeleteOutline, RemoveCircleOutlineOutlined, ReplayOutlined} from "@mui/icons-material";
 
 const Poll = () => {
-  const { boardId, poll, getBoard } = useBoard();
+  const {boardId, poll, getBoard} = useBoard();
   const [open, setOpen] = useState(false);
   const [polls, setPolls] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const openPopover = Boolean(anchorEl);
   const openAddPoll = () => {
     setOpen(true);
   };
@@ -19,12 +23,22 @@ const Poll = () => {
   };
 
   const handlClick = (e) => {
-    if (e.type === 'click') {
-      console.log('Left click');
-    } else if (e.type === 'contextmenu') {
+    e.preventDefault();
+    if (e.type === 'contextmenu') {
       console.log('Right click');
+      optionClickHandler(e);
     }
   }
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  const handleOption = () => {
+    setAnchorEl(null);
+  };
+  const optionClickHandler = (event) => {
+    console.log("fusda");
+    setAnchorEl(event.currentTarget);
+  };
 
   useEffect(() => {
     getBoard();
@@ -46,28 +60,89 @@ const Poll = () => {
   }, [poll]);
 
   return (
-    <div className="poll_container" onClick={handlClick}>
+    <div className="poll_container">
       <Modal
         open={open}
         onClose={closeAddPoll}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <AddPoll handleClose={closeAddPoll} />
+        <AddPoll handleClose={closeAddPoll}/>
       </Modal>
+      <div onContextMenu={(e) => e.preventDefault()}>
+        <Popover
+          id={crypto.randomUUID()}
+          open={openPopover}
+          anchorEl={anchorEl}
+          onClose={handleOption}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          sx={{
+            [".MuiPopover-paper"]: {
+              backgroundColor: "#001e3c55",
+            }
+          }}
+        >
+          <div className="poll_option">
+            <div className="poll_option-button-container">
+              <Button
+                onClick={() => {
+                  setIsOpen(true);
+                }}
+                sx={{color: "#fff", width: "100%", height: "3rem"}}
+              >
+                <div className="poll_option-in-button">
+                  <ReplayOutlined  sx={{fill:"#1976d2", fontSize: "1.5rem"}}/>
+                  <div>برداشتن رای</div>
+                </div>
+              </Button>
+              <Button
+                onClick={() => {
+                  setIsOpen(true);
+                }}
+                sx={{color: "#fff", width: "100%", height: "3rem"}}
+              >
+                <div className="poll_option-in-button">
+                  <RemoveCircleOutlineOutlined sx={{fill:"#1976d2", fontSize: "1.5rem"}}/>
+                  <div>اتمام رای گیری</div>
+                </div>
+              </Button>
+              <Button
+              onClick={() => {
+                setIsOpen(true);
+              }}
+              sx={{color: "#fff", width: "100%", height: "3rem"}}
+            >
+              <div className="poll_option-in-button">
+                <DeleteOutline  sx={{fill:"#1976d2", fontSize: "1.5rem"}}/>
+                <div>پاک کردن رای گیری</div>
+              </div>
+            </Button>
+            </div>
+          </div>
+        </Popover>
+      </div>
       <div className="poll_view">
         <div className="poll_open">
           {polls.map((x) => {
             if (x.is_open) {
               console.log(poll);
               return (
-                <PollView
-                  pollId={x.id}
-                  Multi={x.is_multianswer}
-                  Anonymous={x.is_known}
-                  isOpen
-                  question={x.question}
-                />
+                <div onContextMenu={handlClick}>
+                  <PollView
+                    pollId={x.id}
+                    Multi={x.is_multianswer}
+                    Anonymous={x.is_known}
+                    isOpen
+                    question={x.question}
+                  />
+                </div>
               );
             }
             return null;
@@ -92,7 +167,7 @@ const Poll = () => {
       </div>
       <div className="poll_button">
         <Fab color="primary" aria-label="add" onClick={openAddPoll}>
-          <AddIcon />
+          <AddIcon/>
         </Fab>
       </div>
     </div>
