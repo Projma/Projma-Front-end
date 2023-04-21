0f4d9fdb7118763a9d85da8a37658c9bb25868b9
Vahid Mohammdi <vahidood@gmail.com>
2023-04-21 21:30:21 +0330

    feat: add question and multi or single option

--- src/components/Poll/PollView.js
+++ src/components/Poll/PollView.js
@@ -1,34 +1,31 @@
-import {Typography} from "@mui/material";
-import HowToVoteIcon from '@mui/icons-material/HowToVote';
+import { Typography } from "@mui/material";
+import HowToVoteIcon from "@mui/icons-material/HowToVote";
 import "./PollView.css";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import AnonymousVoter from "./Content/AnonymousVoter";
 import SingleVote from "./Content/SingleVote";
 import MultiVote from "./Content/MultiVote";
 import { HowToRegOutlined } from "@mui/icons-material";
 
-const PollView = () => {
-  const data = {question: "Are you Gay",options: ["Yes","No","Dont know"]};
-  const [tVote, setTVote] = useState(0);
-  useEffect(() => {
-  }, [tVote]);
+const PollView = ({Multi, question, isOpen, Anonymous, pollId}) => {
+
   return (
     <div className="poll_pollview-container">
       <div className="poll_pollview-label">
-        <Typography>{data.question}</Typography>
+        <Typography>{question}</Typography>
       </div>
       <div className="poll_pollview-attendents">
-        <AnonymousVoter/>
+        <AnonymousVoter />
       </div>
       <div className="poll_pollview-options">
-        <MultiVote op={data.options} setTVote={setTVote}/>
+        {Multi ? <MultiVote op={["Yes","No"]} /> : <SingleVote op={["Yes","No"]} />}
       </div>
       <div className="poll_pollview-results">
-        <Typography fontSize="1.1rem">{tVote}</Typography>
-        <HowToRegOutlined sx={{width:"1.5rem",height:"1.5rem"}}/>
+        <Typography fontSize="1.1rem">x</Typography>
+        <HowToRegOutlined sx={{ width: "1.5rem", height: "1.5rem" }} />
       </div>
     </div>
   );
-}
+};
 
 export default PollView;
