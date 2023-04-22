8dbade49504e8d48a20195b5f51d136139c7cfe9
Vahid Mohammdi <vahidood@gmail.com>
2023-04-22 06:50:16 +0330

    feat: get avatar pic from back

--- src/components/Poll/Content/KnownVoter.js
+++ src/components/Poll/Content/KnownVoter.js
@@ -1,30 +1,57 @@
-import AvatarGroup from "@mui/material/AvatarGroup";
-import Tooltip from "@mui/material/Tooltip";
-import Avatar from "@mui/material/Avatar";
+import AvatarGroup from '@mui/material/AvatarGroup';
+import Tooltip from '@mui/material/Tooltip';
+import Avatar from '@mui/material/Avatar';
+import {useEffect, useState} from 'react';
+import apiInstance from '../../../utilities/axiosConfig';
+import {useParams} from 'react-router-dom';
 
 const KnownVoter = ({voters}) => {
-  console.log(voters);
-  return ( <>
+  // const [member, setMember] = useState([]);
+  const [voter, setVoter] = useState(new Array(voters.length));
+  const param = useParams();
+  useEffect(() => {
+    const getMember = async () => {
+      await apiInstance.get(`board/${param.boardId}/members/`).then(res => {
+        // setMember(res.data);
+        setVoter(voters.map(x => {
+          let fn, ln, pp;
+          res.data.forEach(y => {
+            if (x.user__pk === y.user.id) {
+              fn = y.user.first_name;
+              ln = y.user.last_name;
+              pp = y.profile_pic;
+              // console.log("inbaz",fn,ln,pp);
+            }
+          });
+          return {first_name: fn, last_name: ln, profile_pic: pp};
+        }));
+        // console.log('member', res.data);
+      });
+    };
+    getMember();
+  }, [voters]);
+  // console.log(voter,voters);
+  return (<>
     <AvatarGroup
       max={5}
-      spacing="-1"
-      sx={{ direction: "ltr", border: "none" }}
+      spacing="6"
+      sx={{direction: 'ltr', border: 'none'}}
       className="card_avatar-container"
     >
-      {voters.map((x) => (
-        <Tooltip title={x.first_name + " " + x.last_name}>
+      {voter.map((x) => (
+        <Tooltip title={x.first_name + ' ' + x.last_name}>
           <Avatar
             key={crypto.randomUUID()}
-            alt={x.first_name + " " + x.last_name}
-            src={x.profile_pic !== null ? x.profile_pic : "none"}
-            {...stringAvatar(x.first_name + " " + x.last_name)}
+            alt={x.first_name + ' ' + x.last_name}
+            src={x.profile_pic !== null ? x.profile_pic : 'none'}
+            {...stringAvatar(x.first_name + ' ' + x.last_name)}
             className="card_avatar-profile-picture"
           />
         </Tooltip>
       ))}
     </AvatarGroup>
-    </> );
-}
+  </>);
+};
 
 function stringToColor(string) {
   let hash = 0;
@@ -35,7 +62,7 @@ function stringToColor(string) {
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
 
-  let color = "#";
+  let color = '#';
 
   for (i = 0; i < 3; i += 1) {
     const value = (hash >> (i * 8)) & 0xff;
@@ -51,9 +78,8 @@ function stringAvatar(name) {
     sx: {
       bgcolor: stringToColor(name),
     },
-    children: `${name.split(" ")[0][0].toUpperCase()}${name
-      .split(" ")[1][0]
-      .toUpperCase()}`,
+    children: `${name.split(' ')[0][0].toUpperCase()}${name.split(' ')[1][0].toUpperCase()}`,
   };
 }
+
 export default KnownVoter;
