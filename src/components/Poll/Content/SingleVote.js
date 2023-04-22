4a796d95152067be3b54052b6463ee3ef630a77d
Vahid Mohammdi <vahidood@gmail.com>
2023-04-22 06:50:54 +0330

    feat: single vote now work with backend data

--- src/components/Poll/Content/SingleVote.js
+++ src/components/Poll/Content/SingleVote.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
-import "./SingleVote.css";
-import FormControl from "@mui/material/FormControl";
-import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
-import LinearProgress from "@mui/material/LinearProgress";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import Typography from "@mui/material/Typography";
-import { TaskAltOutlined } from "@mui/icons-material";
+import React, {useEffect, useState} from 'react';
+import './SingleVote.css';
+import FormControl from '@mui/material/FormControl';
+import {FormControlLabel, Radio, RadioGroup} from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import {TaskAltOutlined} from '@mui/icons-material';
+import apiInstance from '../../../utilities/axiosConfig';
 
-const SingleVoted = ({ op, setVote }) => {
+const SingleVoted = ({options, setVote}) => {
   return (
     <div className="single-vote_voted">
       <FormControl
         sx={{
-          ".MuiFormControlLabel-label": {
-            fontSize: "1rem",
+          '.MuiFormControlLabel-label': {
+            fontSize: '1rem',
           },
-          ".MuiFormControlLabel-root": {
-            margin: "0 0",
+          '.MuiFormControlLabel-root': {
+            margin: '0 0',
           },
         }}
         variant="standard"
@@ -24,26 +24,32 @@ const SingleVoted = ({ op, setVote }) => {
         <RadioGroup
           aria-labelledby="demo-error-radios"
           name="vote"
-          onChange={(event) => setVote(event.target.value)}
+          onChange={async (event) => {
+            const id = event.target.id.split('|');
+            await apiInstance.post(`board/poll-answers/${id[0]}/vote/`, {
+              text: event.target.value,
+              poll: id[1] / 1,
+            }).then((res) => setVote(event.target.value));
+          }}
         >
-          {op.map((o) => (
+          {options.map((o) => (
             <FormControlLabel
-
-              value={o}
+              value={o.text}
               control={
                 <Radio
+                  id={o.id + '|' + o.poll}
                   sx={{
-                    color: "#5090D3",
-                    fontSize: "1rem",
-                    "& .MuiSvgIcon-root": {
-                      height: "1.5rem",
-                      width: "1.5rem",
+                    color: '#5090D3',
+                    fontSize: '1rem',
+                    '& .MuiSvgIcon-root': {
+                      height: '1.5rem',
+                      width: '1.5rem',
                     },
                   }}
                 />
               }
-              label={<Typography fontSize={"1.2rem"}>{o}</Typography>}
-              sx={{ height: "4rem", width: "100%", padding: "0.5rem" }}
+              label={<Typography fontSize={'1.2rem'}>{o.text}</Typography>}
+              sx={{height: '4rem', width: '100%', padding: '0.5rem'}}
             />
           ))}
         </RadioGroup>
@@ -52,51 +58,46 @@ const SingleVoted = ({ op, setVote }) => {
   );
 };
 
-const getPercent = (vote, all) => {
-  let per = 0;
-  all.forEach((x) => {
-    if (vote === x.vote) per++;
-  });
-  return Math.trunc((per / all.length) * 100);
+const getPercent = (vote, total) => {
+  return Math.trunc((vote / total) * 100);
 };
 
-const SingleVoteResult = ({ vote, op, userVote }) => {
-  // const [percentage, setPercentage] = useState(0);
+const SingleVoteResult = ({options,totalVotes}) => {
   return (
     <div className="single-vote_result">
-      {op.map((v, i) => (
+      {options.map((v, i) => (
         <div className="single-vote_result-container">
           <div className="single-vote_result-icon">
-            {userVote === v && (
+            {v.is_user_voted === true && (
               <TaskAltOutlined
-                sx={{ width: "1.5rem", height: "1.5rem", color: "#5090d3" }}
+                sx={{width: '1.5rem', height: '1.5rem', color: '#5090d3'}}
               />
             )}
           </div>
           <div className="single-vote_result-progressbar">
             <LinearProgress
               variant="buffer"
-              value={getPercent(v, vote)}
+              value={getPercent(v.count, totalVotes)}
               valueBuffer={100}
               sx={{
                 height: 6,
-                width: "100%",
-                maxWidth: "100%",
+                width: '100%',
+                maxWidth: '100%',
                 rotate: '180deg',
                 borderRadius: 5,
                 [`&.linearProgressClasses`]: {
-                  backgroundColor: "#5090D3",
+                  backgroundColor: '#5090D3',
                 },
                 [`& .linearProgressClasses.bar`]: {
                   borderRadius: 5,
-                  backgroundColor: "#0059B2",
+                  backgroundColor: '#0059B2',
                 },
               }}
             />
           </div>
-          <div className="single-vote_result-title">{v}</div>
+          <div className="single-vote_result-title">{v.text}</div>
           <div className="single-vote_result-percentage">
-            {getPercent(v, vote)}%
+            {getPercent(v.count, totalVotes)}%
           </div>
         </div>
       ))}
@@ -104,23 +105,24 @@ const SingleVoteResult = ({ vote, op, userVote }) => {
   );
 };
 
-const SingleVote = ({ op }) => {
-  // const [isVote, setIsVote] = useState(false);
-  const [vote, setVote] = useState(undefined);
-  const [allVote, setAllVote] = useState([
-    { id: 1, vote: "Yes" },
-    { id: 2, vote: "No" },
-    { id: 3, vote: "Yes" },
-  ]);
-  useEffect(() => {
-    if (vote !== undefined) setAllVote([...allVote, { id: 4, vote }]);
-  }, [vote]);
+const SingleVote = ({options, isOpen,isVoted,totalVotes}) => {
+  // console.log('options', options);
+
+  // console.log("total votes",totalVotes);
   return (
     <div className="single-vote_container">
-      {vote === undefined ? (
-        <SingleVoted op={op} setVote={setVote} />
+      {isOpen === true && isVoted !== true ? (
+        <SingleVoted
+          key={crypto.randomUUID()}
+          options={options}
+          // setVote={setVote}
+        />
       ) : (
-        <SingleVoteResult op={op} vote={allVote} userVote={vote} />
+        <SingleVoteResult
+          key={crypto.randomUUID()}
+          options={options}
+          totalVotes={totalVotes}
+        />
       )}
     </div>
   );
