231f2835bd13fee2f21b5b5bec59f8a8895751cc
Vahid Mohammdi <vahidood@gmail.com>
2023-04-26 23:27:56 +0330

    feat: list card

--- src/components/Retro/content/RetroList.js
+++ src/components/Retro/content/RetroList.js
@@ -1,7 +1,22 @@
-const RetroList = ({children}) => {
-  return <div style={{width:"32rem",height:"100%",backgroundColor:"#0A1929",borderRadius:"1rem",display:"flex",flexFlow:"column",padding:"1rem 1rem"}}>
-    {children}
-  </div>;
+const RetroList = ({ children }) => {
+  return (
+    <div
+      style={{
+        width: "32rem",
+        height: "100%",
+        backgroundColor: "#0A1929",
+        borderRadius: "1rem",
+        display: "flex",
+        flexFlow: "column",
+        padding: "1rem 1rem",
+        // alignItems: "flex-start",
+        justifyContent: "flex-start",
+        gap: "1.5rem"
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default RetroList;
