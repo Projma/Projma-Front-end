f4c57f67a7bae2924623ff87d7dd01ca37a8c311
Vahid Mohammdi <vahidood@gmail.com>
2023-04-26 21:59:22 +0330

    feat: shared main list

---
+++ src/components/Retro/content/RetroList.js
@@ -0,0 +1,7 @@
+const RetroList = ({children}) => {
+  return <div style={{width:"32rem",height:"100%",backgroundColor:"#0A1929",borderRadius:"1rem",display:"flex",flexFlow:"column",padding:"1rem 1rem"}}>
+    {children}
+  </div>;
+};
+
+export default RetroList;
