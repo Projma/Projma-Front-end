43317cd0f99e129e686cad151e5c6224c46f4da9
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2023-05-06 20:31:13 +0430

    [chore]: add test for error handling in create board modal

--- src/components/Workspace_management/CreateBoardModal/CreateBoard.test.jsx
+++ src/components/Workspace_management/CreateBoardModal/CreateBoard.test.jsx
@@ -99,39 +99,29 @@ test("it calls on_submit function with correct parameters after entering the inf
   );
 });
 
-// test("it gives error when trying to create a new board without specifying name for that", async () => {
-//   const onSubmit = vi.fn();
-//   render(
-//     <CreateBoardModal
-//       params={{}}
-//       on_submit={onSubmit}
-//       boards={{}}
-//       setBoards={() => {}}
-//     />
-//   );
-//   const add = document.querySelector("#add_button");
-//   user.click(add);
-//   const name = screen.getByLabelText("نام بورد").toBeInTheDocument();
-//   const description = screen.getByLabelText("توضیحات").toBeInTheDocument();
-//   const file_inp = document
-//     .querySelector("input[type=file]")
-//     .toBeInTheDocument();
-//   const create_button = document
-//     .querySelector("input[type=submit]")
-//     .toBeInTheDocument();
-//   user.click(name);
-//   user.keyboard("test board");
-//   user.click(description);
-//   user.keyboard("test description");
-//   user.click(create_button);
-//   expect(onSubmit).toHaveBeenNthCalledWith(
-//     1,
-//     expect.objectContaining({
-//       name: "test board",
-//       description: "test description",
-//     })
-//   );
-// });
+test("it gives error when trying to create a new board without specifying name for that", async () => {
+  const onSubmit = vi.fn();
+  render(
+    <CreateBoardModal
+      params={{}}
+      on_submit={onSubmit}
+      boards={{}}
+      setBoards={() => {}}
+    />
+  );
+  const add = document.querySelector("#add_button");
+  user.click(add);
+  const name = screen.getByLabelText("نام بورد").toBeInTheDocument();
+  const description = screen.getByLabelText("توضیحات").toBeInTheDocument();
+  const file_inp = document
+    .querySelector("input[type=file]")
+    .toBeInTheDocument();
+  const create_button = document
+    .querySelector("input[type=submit]")
+    .toBeInTheDocument();
+  user.click(create_button);
+  expect(screen.getByText("نام بورد نمی تواند خالی باشد")).toBeInTheDocument();
+});
 
 // test("it puts the related number of predefined repeat durations in the custom repeat duration input", async () => {
 //   render(
