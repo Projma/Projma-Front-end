ba689b0b5569a79a65f054b081695e9d8c5570ab
Vahid Mohammdi <vahidood@gmail.com>
2023-05-06 19:24:29 +0330

    test: number of textfield in reset password

--- src/components/Password/ResetPassword.test.jsx
+++ src/components/Password/ResetPassword.test.jsx
@@ -1,21 +1,24 @@
-import { render, screen } from "@testing-library/react";
-import user from "@testing-library/user-event";
-import ResetPassword from "./ResetPassword";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ResetPassword,{handleSubmit} from "./ResetPassword";
 
-test("show two inputs", async () => {
+test("show two password inputs", async () => {
   render(<ResetPassword />);
   const confirmPassword = screen.getByPlaceholderText(
     /رمز عبور خود را دوباره وارد کنید/i
   );
-  const password = screen.getByPlaceholderText(
-    /رمز عبور خود را وارد کنید/i
-  );
+  const password = screen.getByPlaceholderText(/رمز عبور خود را وارد کنید/i);
   expect(confirmPassword).toBeInTheDocument();
   expect(password).toBeInTheDocument();
 });
 
+test("test number of textfield", () => {
+  render(<ResetPassword />);
+  const inputs = screen.getAllByPlaceholderText(/وارد کنید/i);
+  expect(inputs).toHaveLength(2);
+});
+
 test("show submit button", () => {
   render(<ResetPassword />);
-  const submitButton = screen.getByRole("button");
+  const submitButton = screen.getByRole('button', { name: /تغییر رمز عبور/i });
   expect(submitButton).toBeInTheDocument();
 });
