3dc7afbcb199d9e1723b4d9f9020525dc099fad1
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2023-06-04 12:24:04 +0330

    [test]: test for share button

---
+++ src/components/Board/temp/ShareButton/ShareButton.test.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import ShareButton from './ShareButton';
+
+describe('ShareButton', () => {
+  test('renders ShareButton component', () => {
+    render(<ShareButton />);
+    
+    // Assert that the ShareButton component is rendered
+    const shareButton = screen.getByRole('button', { name: 'اشتراک' });
+    expect(shareButton).toBeInTheDocument();
+  });
+
+  test('opens modal on button click', () => {
+    render(<ShareButton />);
+    
+    // Click the ShareButton
+    const shareButton = screen.getByRole('button', { name: 'اشتراک' });
+    fireEvent.click(shareButton);
+    
+    // Assert that the modal is open
+    const modalTitle = screen.getByText('بورد را به اشتراک بگذارید');
+    expect(modalTitle).toBeInTheDocument();
+  });
+
+  test('closes modal on clear button click', () => {
+    render(<ShareButton />);
+    
+    // Click the ShareButton to open the modal
+    const shareButton = screen.getByRole('button', { name: 'اشتراک' });
+    fireEvent.click(shareButton);
+    
+    // Click the clear button to close the modal
+    const clearButton = screen.getByRole('button', { name: 'clear' });
+    fireEvent.click(clearButton);
+    
+    // Assert that the modal is closed
+    const modalTitle = screen.queryByText('بورد را به اشتراک بگذارید');
+    expect(modalTitle).not.toBeInTheDocument();
+  });
+
+  test('copies invite link on copy button click', async () => {
+    render(<ShareButton />);
+    
+    // Click the ShareButton to open the modal
+    const shareButton = screen.getByRole('button', { name: 'اشتراک' });
+    fireEvent.click(shareButton);
+    
+    // Click the copy button to copy invite link
+    const copyButton = screen.getByRole('button', { name: 'کپی لینک' });
+    fireEvent.click(copyButton);
+    
+    // Wait for the invite link to be copied
+    await screen.findByText('لینک کپی شد.');
+    
+    // Assert that the invite link is copied
+    const inviteLink = screen.getByText('http://localhost:3000/borad_invitation/');
+    expect(inviteLink).toBeInTheDocument();
+  });
+
+  // Add more tests for other functionalities of the ShareButton component
+
+});
