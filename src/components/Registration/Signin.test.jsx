2ddeb1a48951a7d97ceab5a5568ba2f35c82e693
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2023-06-05 13:44:54 +0330

    [test]: resolving error of useTheme in tests

--- src/components/Registration/Signin.test.jsx
+++ src/components/Registration/Signin.test.jsx
@@ -3,6 +3,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { render, screen, fireEvent, getByRole } from "@testing-library/react";
 
 import SignIn from "./Signin";
+import useTheme from "../../hooks/useTheme";
+
+vi.mock("../../hooks/useTheme", () => ({
+  __esModule: true,
+  default: () => ({
+    theme: {
+      name: "sun",
+      mainBg: "#e5e5e5",
+      minorBg: "#fff",
+      secondary: "#f8981c",
+      tertiary: "#f47922",
+      hover: "#f4792280",
+      primary: "#fdb713",
+    },
+    getColor: (bgColor) => "#000000",
+  }),
+}));
 
 test("have input for inputs", () => {
   render(
