af8dca466a32f3d3f729eaaa9b31704feda7851b
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2023-06-05 13:44:46 +0330

    [test]: resolving error of useTheme in tests

--- src/components/Profile/ChangePassword.test.jsx
+++ src/components/Profile/ChangePassword.test.jsx
@@ -11,6 +11,23 @@ import { Provider } from "react-redux";
 import ChangePassword from "./ChangePassword";
 import { MemoryRouter } from "react-router-dom";
 import configureStore from "redux-mock-store";
+import useTheme from "../../hooks/useTheme";
+
+vi.mock("../../hooks/useTheme", () => ({
+  __esModule: true,
+  default: () => ({
+    theme: {
+      name: "sun",
+      mainBg: "#e5e5e5",
+      minorBg: "#fff",
+      secondary: "#f8981c",
+      tertiary: "#f47922",
+      hover: "#f4792280",
+      primary: "#fdb713",
+    },
+    getColor: (bgColor) => "#000000",
+  }),
+}));
 
 test("number of textfield", () => {
   const initialState = {
