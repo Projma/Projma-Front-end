470d632524ce66b888b3347a90ae08d9288f3027
Sina Alinejad <sina_alinejad@comp.iust.ac.ir>
2023-06-05 13:46:06 +0330

    [test]: resolving error of useTheme in tests

--- src/components/WorkspaceManagement/CreateBoardModal/CreateBoard.test.jsx
+++ src/components/WorkspaceManagement/CreateBoardModal/CreateBoard.test.jsx
@@ -3,6 +3,23 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
 import user from "@testing-library/user-event";
 import CreateBoardModal from "./CreateBoard";
+import useTheme from "../../../hooks/useTheme";
+
+vi.mock("../../../hooks/useTheme", () => ({
+  __esModule: true,
+  default: () => ({
+    theme: {
+      name: "sun",
+      mainBg: "#e5e5e5",
+      minorBg: "#fff",
+      secondary: "#f8981c",
+      tertiary: "#f47922",
+      hover: "#f4792280",
+      primary: "#fdb713",
+    },
+    getColor: (bgColor) => "#000000",
+  }),
+}));
 
 test("it shows an input for name, an input for description, another input for choosing file and also a button for creating", async () => {
   const onSubmit = vi.fn();
