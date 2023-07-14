d38274895779fe54bc44724993517c69c0ec95f8
Vahid Mohammdi <vahidood@gmail.com>
2023-07-15 03:02:10 +0330

    feat: create modal component

---
+++ src/components/Asset/Modal.jsx
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useMemo, useRef } from "react";
+import styles from "./Modal.module.scss";
+import useTheme from "../../hooks/useTheme";
+
+const Modal = ({ open, locked, onClose, children, ...props }) => {
+  const modalRef = useRef(null);
+  const { theme, getColor } = useTheme();
+
+  const dialogClasses = useMemo(() => {
+    const _arr = [styles["modal"]];
+    if (!open) _arr.push(styles["modal--closing"]);
+    return _arr.join(" ");
+  }, [open]);
+
+  const onCancel = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!locked) onClose();
+    },
+    [locked, onClose]
+  );
+
+  const onClick = useCallback(
+    ({ target }) => {
+      const { current: el } = modalRef;
+      if (target === el && !locked) onClose();
+    },
+    [locked, onClose]
+  );
+
+  const onAnimEnd = useCallback(() => {
+    const { current: el } = modalRef;
+    if (!open) el.close();
+  }, [open]);
+
+  useEffect(() => {
+    const { current: el } = modalRef;
+    if (open) el.showModal();
+  }, [open]);
+
+  return (
+    <dialog
+      ref={modalRef}
+      className={dialogClasses}
+      onClose={onClose}
+      onCancel={onCancel}
+      onClick={onClick}
+      onAnimationEnd={onAnimEnd}
+    >
+      <div
+        className={styles["modal__container"]}
+        style={{ color: getColor(theme.minorBg) }}
+      >
+        {children}
+      </div>
+    </dialog>
+  );
+};
+
+export default Modal;
