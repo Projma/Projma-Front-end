f5e0ace443bd36cbe98d013eccf5f943468c084b
Vahid Mohammdi <vahidood@gmail.com>
2023-07-24 09:50:12 +0330

    chore: change constant

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,12 +2,12 @@
 // import createBrowserHistory from "history/createBrowserHistory";
 // export const history = createBrowserHistory();
 
-export const swaggerUrl = "http://127.0.0.1:8000/swagger/";
-export const baseUrl = "http://127.0.0.1:8000/";
-export const baseUrlForSocket = "ws://localhost:8000";
-export const baseUrlFront = "http://localhost:3000/";
+// export const swaggerUrl = "http://127.0.0.1:8000/swagger/";
+// export const baseUrl = "http://127.0.0.1:8000/";
+// export const baseUrlForSocket = "ws://localhost:8000";
+// export const baseUrlFront = "http://localhost:3000/";
 
-// export const swaggerUrl = "http://45.147.97.94:1337/swagger/";
-// export const baseUrl = "http://45.147.97.94:1337/";
-// export const baseUrlForSocket = "ws://45.147.97.94:1337";
-// export const baseUrlFront = "http://45.147.97.94/";
+export const swaggerUrl = "http://45.147.97.94:1337/swagger/";
+export const baseUrl = "http://45.147.97.94:1337/";
+export const baseUrlForSocket = "ws://45.147.97.94:1337";
+export const baseUrlFront = "http://45.147.97.94/";
